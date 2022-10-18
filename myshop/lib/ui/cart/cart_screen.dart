1dda7b7986a0793daca673e311a9b567028f86ae
Maihong01 <hongb1910520@student.ctu.edu.vn>
2022-10-19 01:09:26 +0700

    Quan ly trang thai voi provider - OrdersManager

--- myshop/lib/ui/cart/cart_screen.dart
+++ myshop/lib/ui/cart/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../orders/order_manager.dart';
 import 'cart_manager.dart';
 import 'cart_item_card.dart';
 
@@ -64,14 +65,20 @@ class CartScreen extends StatelessWidget {
               backgroundColor: Theme.of(context).primaryColor,
             ),
             TextButton(
-              onPressed: () {
-                print('An order has been added.');
-              },
+              onPressed: cart.totalAmount <= 0
+                  ? null
+                  : () {
+                      context.read<OrdersManager>().addOrder(
+                            cart.products,
+                            cart.totalAmount,
+                          );
+                      cart.clear();
+                    },
               style: TextButton.styleFrom(
                 textStyle: TextStyle(color: Theme.of(context).primaryColor),
               ),
-              child: const Text('Order now'),
-            )
+              child: const Text('ORDER NOW'),
+            ),
           ],
         ),
       ),

adf4fa468b4ac2353e2fafdea4a754c3b654a1be
Maihong01 <hongb1910520@student.ctu.edu.vn>
2022-10-18 23:14:00 +0700

    Quan ly trang thai voi provider - ProductsManager

--- myshop/lib/main.dart
+++ myshop/lib/main.dart
@@ -9,6 +9,8 @@ import 'ui/products/product_manager.dart';
 import 'ui/products/product_overview_screen.dart';
 import 'ui/products/user_products_screen.dart';
 
+import 'package:provider/provider.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -18,34 +20,41 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Shop',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          fontFamily: 'Lato',
-          colorScheme:
-              ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(
-            secondary: Colors.deepOrange,
-          )),
-      home: const ProductsOverViewScreen(),
-      routes: {
-        CartScreen.routeName: (ctx) => const CartScreen(),
-        OrdersScreen.routeName: (ctx) => const OrdersScreen(),
-        UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == ProductDetailScreen.routeName) {
-          final productId = settings.arguments as String;
-          return MaterialPageRoute(
-            builder: (ctx) {
-              return ProductDetailScreen(
-                ProductManager().findById(productId),
-              );
-            },
-          );
-        }
-        return null;
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => ProductManager(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'My Shop',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            fontFamily: 'Lato',
+            colorScheme:
+                ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(
+              secondary: Colors.deepOrange,
+            )),
+        home: const ProductsOverViewScreen(),
+        routes: {
+          CartScreen.routeName: (ctx) => const CartScreen(),
+          OrdersScreen.routeName: (ctx) => const OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
+        },
+        onGenerateRoute: (settings) {
+          if (settings.name == ProductDetailScreen.routeName) {
+            final productId = settings.arguments as String;
+            return MaterialPageRoute(
+              builder: (ctx) {
+                return ProductDetailScreen(
+                  ctx.read<ProductManager>().findById(productId),
+                );
+              },
+            );
+          }
+          return null;
+        },
+      ),
     );
   }
 }
--- myshop/lib/ui/products/products_grid.dart
+++ myshop/lib/ui/products/products_grid.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-
-import 'product_grid_tile.dart';
+import 'product_grid_tile.dart ';
 import 'product_manager.dart';
+import '../../models/product.dart';
+import 'package:provider/provider.dart';
 
 class ProductsGrid extends StatelessWidget {
   final bool showFavorites;
@@ -10,13 +11,15 @@ class ProductsGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productsManager = ProductManager();
-    final products =
-        showFavorites ? productsManager.favoriteItems : productsManager.items;
+    final products = context.select<ProductManager, List<Product>>(
+        (productsManager) => showFavorites
+            ? productsManager.favoriteItems
+            : productsManager.items);
+
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: ((ctx, i) => ProductGridTile(products[i])),
+      itemBuilder: (ctx, i) => ProductGridTile(products[i]),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 3 / 2,

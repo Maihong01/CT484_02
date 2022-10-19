a6864f8ef84b1eb593596715b4d7197f725209a2
Maihong01 <hongb1910520@student.ctu.edu.vn>
2022-10-19 17:32:43 +0700

    Xay dung trang hieu chinh san pham

--- myshop/lib/main.dart
+++ myshop/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myshop/ui/orders/orders_screen.dart';
+import 'package:myshop/ui/products/edit_product_screen.dart';
 
 import 'package:myshop/ui/products/user_products_screen.dart';
 import 'package:provider/provider.dart';
@@ -56,6 +57,18 @@ class MyApp extends StatelessWidget {
               builder: (ctx) {
                 return ProductDetailScreen(
                   ctx.read<ProductManager>().findById(productId),
+                );
+              },
+            );
+          }
+          if (settings.name == EditProductScreen.routeName) {
+            final productId = settings.arguments as String?;
+            return MaterialPageRoute(
+              builder: (ctx) {
+                return EditProductScreen(
+                  productId != null
+                      ? ctx.read<ProductManager>().findById(productId)
+                      : null,
                 );
               },
             );
